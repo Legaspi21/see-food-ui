d8372b745fbeb8891f202b7987cb8cb193e61a3a
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-03-14 23:56:01 -0700

    Set up Actions directory with a registration action to handle an email and password

---
+++ src/actions/RegistrationActions.js
@@ -0,0 +1,14 @@
+import { 
+	EMAIL_CHANGED, 
+	PASSWORD_CHANGED, 
+} from './types';
+
+export const emailChanged = (text) => ({
+		type: EMAIL_CHANGED,
+		payload: text
+});
+
+export const passwordChanged = (text) => ({
+		type: PASSWORD_CHANGED,
+		payload: text
+});
